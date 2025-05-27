53aa97011c3e6bfbd0398ebc7aae201ab3ecdab3
hyun2y00 <pottq577@naver.com>
2025-05-27 10:31:11 +0900

    chore: 초기 빌드 및 gitignore 추가

---
+++ src/main/java/com/sparta/spartatigers/SpartaTigersApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartatigers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaTigersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaTigersApplication.class, args);
+    }
+
+}
