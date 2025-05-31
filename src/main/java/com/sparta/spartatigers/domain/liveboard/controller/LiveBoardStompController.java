a6df18108150d844633bb49a91ce9cf9e245453b
김정민 <raon37910@gmail.com>
2025-05-31 13:40:29 +0900

    refactor: LiveBoardStompController API 경로 수정

--- src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardStompController.java
+++ src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardStompController.java
@@ -1,22 +1,20 @@
 package com.sparta.spartatigers.domain.liveboard.controller;
 
+import com.sparta.spartatigers.domain.liveboard.model.LiveBoardMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import lombok.RequiredArgsConstructor;
-
-import com.sparta.spartatigers.domain.liveboard.model.LiveBoardMessage;
 
 @Controller
 @RequiredArgsConstructor
 public class LiveBoardStompController {
 
-    private final SimpMessagingTemplate messagingTemplate;
+	private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/chat/message")
-    public void sendMessage(LiveBoardMessage message) {
-        String roomId = message.getRoomId();
-        messagingTemplate.convertAndSend("sub/chat/room" + roomId, message);
-    }
+	@MessageMapping("/chat/message")
+	public void sendMessage(LiveBoardMessage message) {
+		String roomId = message.getRoomId();
+		messagingTemplate.convertAndSend("client/chat/room" + roomId, message);
+	}
 }
