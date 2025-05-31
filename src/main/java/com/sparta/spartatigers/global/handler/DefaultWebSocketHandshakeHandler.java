d294f8327c32dbacb1f2523a3ac0878f1df6500e
김정민 <raon37910@gmail.com>
2025-05-31 10:18:09 +0900

    feat: formatting

--- src/main/java/com/sparta/spartatigers/global/handler/DefaultWebSocketHandshakeHandler.java
+++ src/main/java/com/sparta/spartatigers/global/handler/DefaultWebSocketHandshakeHandler.java
@@ -1,9 +1,11 @@
 package com.sparta.spartatigers.global.handler;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.Map;
 import java.util.UUID;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
@@ -16,22 +18,22 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
  */
 public class DefaultWebSocketHandshakeHandler extends DefaultHandshakeHandler {
 
-	@Override
-	protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
-		Map<String, Object> attributes) {
+    @Override
+    protected Principal determineUser(
+            ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
 
-		if (request instanceof ServletServerHttpRequest serverHttpRequest) {
-			HttpServletRequest servletRequest = serverHttpRequest.getServletRequest();
-			// TODO Security 인증이 도입되었을 때 별도의 인터셉터를 통해서 수동으로 인증 객체를 만들어 줘야할 가능성이 있음.
-			// Spring Security라면 여기서 Security에 의해 세팅된 유저 정보가 나온다고한다.
-			Principal userPrincipal = servletRequest.getUserPrincipal();
-			if (userPrincipal != null) {
-				// 회원인 경우
-				return userPrincipal;
-			}
-		}
+        if (request instanceof ServletServerHttpRequest serverHttpRequest) {
+            HttpServletRequest servletRequest = serverHttpRequest.getServletRequest();
+            // TODO Security 인증이 도입되었을 때 별도의 인터셉터를 통해서 수동으로 인증 객체를 만들어 줘야할 가능성이 있음.
+            // Spring Security라면 여기서 Security에 의해 세팅된 유저 정보가 나온다고한다.
+            Principal userPrincipal = servletRequest.getUserPrincipal();
+            if (userPrincipal != null) {
+                // 회원인 경우
+                return userPrincipal;
+            }
+        }
 
-		String sessionId = UUID.randomUUID().toString();
-		return () -> sessionId;
-	}
+        String sessionId = UUID.randomUUID().toString();
+        return () -> sessionId;
+    }
 }
