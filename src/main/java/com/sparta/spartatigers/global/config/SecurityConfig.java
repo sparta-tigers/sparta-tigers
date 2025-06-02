6d916db7cb85a9d7e3e22c9efe64f49665be7e9b
aksirjwn <skks1000@naver.com>
2025-06-02 12:24:49 +0900

    fix : session Policy 수정

--- src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .sessionManagement(
+                        sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(
                         auth ->
                                 auth.requestMatchers("/auth/**", "/oauth2/**", "/login/**")
