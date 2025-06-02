9cc61b90e63eea2cc808b832d6f625afa576f110
xaehub <novxae@gmail.com>
2025-06-02 20:17:22 +0900

    fix: Apiresponse 응답 형식

--- src/main/java/com/sparta/spartatigers/domain/chatroom/controller/DirectRoomController.java
+++ src/main/java/com/sparta/spartatigers/domain/chatroom/controller/DirectRoomController.java
@@ -1,31 +1,27 @@
 package com.sparta.spartatigers.domain.chatroom.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
-
 import com.sparta.spartatigers.domain.chatroom.dto.request.CreateDirectRoomRequestDto;
 import com.sparta.spartatigers.domain.chatroom.dto.response.DirectRoomDto;
 import com.sparta.spartatigers.domain.chatroom.service.DirectRoomService;
 import com.sparta.spartatigers.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/direct-rooms")
 @RequiredArgsConstructor
 public class DirectRoomController {
 
-    private final DirectRoomService directRoomService;
+	private final DirectRoomService directRoomService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<DirectRoomDto>> createDirectRoom(
-            @RequestBody CreateDirectRoomRequestDto request) {
-        DirectRoomDto directRoomDto = directRoomService.createRoom(request.getExchangeRequestId());
-        ApiResponse<DirectRoomDto> response = ApiResponse.created(directRoomDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
+	@PostMapping
+	public ApiResponse<DirectRoomDto> createDirectRoom(
+		@Valid @RequestBody CreateDirectRoomRequestDto request) {
+		DirectRoomDto directRoomDto = directRoomService.createRoom(request.getExchangeRequestId());
+		return ApiResponse.created(directRoomDto);
+	}
 }
