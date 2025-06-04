9f6e000d50e1c5127e053e066205ea05a192d1b6
xaehub <novxae@gmail.com>
2025-06-04 09:51:41 +0900

    feat (1대1 채팅방): 현재 로그인한 유저의 채팅방 목록 조회 - 유저 정보를 가져와야함

--- src/main/java/com/sparta/spartatigers/domain/chatroom/repository/DirectRoomRepository.java
+++ src/main/java/com/sparta/spartatigers/domain/chatroom/repository/DirectRoomRepository.java
@@ -2,12 +2,20 @@ package com.sparta.spartatigers.domain.chatroom.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sparta.spartatigers.domain.chatroom.model.entity.DirectRoom;
 import com.sparta.spartatigers.domain.exchangerequest.model.entity.ExchangeRequest;
+import com.sparta.spartatigers.domain.user.model.entity.User;
 
 public interface DirectRoomRepository extends JpaRepository<DirectRoom, Long> {
 
     Optional<DirectRoom> findByExchangeRequest(ExchangeRequest exchangeRequest);
+
+    @Query("SELECT r FROM direct_rooms r WHERE r.sender = :user OR r.receiver = :user")
+    Page<DirectRoom> findAllByUser(@Param("user") User user, Pageable pageable);
 }
--- src/main/java/com/sparta/spartatigers/domain/chatroom/service/DirectRoomService.java
+++ src/main/java/com/sparta/spartatigers/domain/chatroom/service/DirectRoomService.java
@@ -1,5 +1,7 @@
 package com.sparta.spartatigers.domain.chatroom.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +12,7 @@ import com.sparta.spartatigers.domain.chatroom.model.entity.DirectRoom;
 import com.sparta.spartatigers.domain.chatroom.repository.DirectRoomRepository;
 import com.sparta.spartatigers.domain.exchangerequest.model.entity.ExchangeRequest;
 import com.sparta.spartatigers.domain.exchangerequest.repository.ExchangeRequestRepository;
+import com.sparta.spartatigers.domain.user.model.entity.User;
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +33,10 @@ public class DirectRoomService {
                                         directRoomRepository.save(
                                                 DirectRoom.create(exchangeRequest)));
         return DirectRoomDto.from(room);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<DirectRoomDto> getRoomsForUser(User currentUser, Pageable pageable) {
+        return directRoomRepository.findAllByUser(currentUser, pageable).map(DirectRoomDto::from);
     }
 }
