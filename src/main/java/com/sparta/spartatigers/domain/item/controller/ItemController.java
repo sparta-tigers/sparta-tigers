d8e5e8a6e39836479ab6401201cd1657d59a0282
anyubinn <anyubin0724@naver.com>
2025-06-04 15:38:58 +0900

    fix: 린트 깨진 것 수정 #25

--- src/main/java/com/sparta/spartatigers/domain/item/controller/ItemController.java
+++ src/main/java/com/sparta/spartatigers/domain/item/controller/ItemController.java
@@ -1,31 +1,34 @@
 package com.sparta.spartatigers.domain.item.controller;
 
-import com.sparta.spartatigers.domain.item.dto.request.CreateItemRequestDto;
-import com.sparta.spartatigers.domain.item.service.ItemService;
-import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
-import com.sparta.spartatigers.global.response.ApiResponse;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+import com.sparta.spartatigers.domain.item.dto.request.CreateItemRequestDto;
+import com.sparta.spartatigers.domain.item.service.ItemService;
+import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
+import com.sparta.spartatigers.global.response.ApiResponse;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/items")
 public class ItemController {
 
-	private final ItemService itemService;
+    private final ItemService itemService;
 
-	@PostMapping
-	public ApiResponse<Void> createItem(
-		@Valid @RequestBody CreateItemRequestDto request,
-		@AuthenticationPrincipal CustomUserPrincipal userPrincipal) {
+    @PostMapping
+    public ApiResponse<Void> createItem(
+            @Valid @RequestBody CreateItemRequestDto request,
+            @AuthenticationPrincipal CustomUserPrincipal userPrincipal) {
 
-		itemService.createItem(request, userPrincipal);
+        itemService.createItem(request, userPrincipal);
 
-		return ApiResponse.created(null);
-	}
+        return ApiResponse.created(null);
+    }
 }
