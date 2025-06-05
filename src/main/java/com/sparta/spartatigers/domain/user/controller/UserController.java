fb9e9394333fbea5b9645de5ad29f2083d8cd2f4
aksirjwn <skks1000@naver.com>
2025-06-05 10:50:37 +0900

    refactor : 소셜 로그인 로직 수정

--- src/main/java/com/sparta/spartatigers/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/spartatigers/domain/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.sparta.spartatigers.domain.user.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
