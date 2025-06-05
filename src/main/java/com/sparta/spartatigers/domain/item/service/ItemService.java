6fd652f92b100e5f4b1b12f8cedb46763cbddc8e
anyubinn <anyubin0724@naver.com>
2025-06-05 20:11:15 +0900

    refactor: 린트 수정 #27

--- src/main/java/com/sparta/spartatigers/domain/item/service/ItemService.java
+++ src/main/java/com/sparta/spartatigers/domain/item/service/ItemService.java
@@ -1,5 +1,12 @@
 package com.sparta.spartatigers.domain.item.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
 import com.sparta.spartatigers.domain.item.dto.request.CreateItemRequestDto;
 import com.sparta.spartatigers.domain.item.dto.response.CreateItemResponseDto;
 import com.sparta.spartatigers.domain.item.dto.response.ReadItemDetailResponseDto;
@@ -9,47 +16,42 @@ import com.sparta.spartatigers.domain.item.model.entity.Item.Status;
 import com.sparta.spartatigers.domain.item.repository.ItemRepository;
 import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
 import com.sparta.spartatigers.domain.user.model.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
-	private final ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
-	@Transactional
-	public CreateItemResponseDto createItem(
-		CreateItemRequestDto request, CustomUserPrincipal principal) {
+    @Transactional
+    public CreateItemResponseDto createItem(
+            CreateItemRequestDto request, CustomUserPrincipal principal) {
 
-		User user = principal.getUser();
+        User user = principal.getUser();
 
-		Item item = Item.of(request, user);
-		itemRepository.save(item);
+        Item item = Item.of(request, user);
+        itemRepository.save(item);
 
-		return CreateItemResponseDto.from(item);
-	}
+        return CreateItemResponseDto.from(item);
+    }
 
-	@Transactional(readOnly = true)
-	public Page<ReadItemResponseDto> findAllItems(
-		CustomUserPrincipal principal, Pageable pageable) {
+    @Transactional(readOnly = true)
+    public Page<ReadItemResponseDto> findAllItems(
+            CustomUserPrincipal principal, Pageable pageable) {
 
-		User user = principal.getUser();
-		// TODO 프론트와 연결 후 실시간으로 로그인한 회원과 가까운 위치에 있는 글이 조회되도록 하기
+        User user = principal.getUser();
+        // TODO 프론트와 연결 후 실시간으로 로그인한 회원과 가까운 위치에 있는 글이 조회되도록 하기
 
-		Page<Item> itemList = itemRepository.findAllByStatus(Status.REGISTERED, pageable);
+        Page<Item> itemList = itemRepository.findAllByStatus(Status.REGISTERED, pageable);
 
-		return itemList.map(ReadItemResponseDto::from);
-	}
+        return itemList.map(ReadItemResponseDto::from);
+    }
 
-	@Transactional(readOnly = true)
-	public ReadItemDetailResponseDto findItemById(Long itemId) {
+    @Transactional(readOnly = true)
+    public ReadItemDetailResponseDto findItemById(Long itemId) {
 
-		Item item = itemRepository.findItemByIdOrElseThrow(itemId);
+        Item item = itemRepository.findItemByIdOrElseThrow(itemId);
 
-		return ReadItemDetailResponseDto.from(item);
-	}
+        return ReadItemDetailResponseDto.from(item);
+    }
 }
