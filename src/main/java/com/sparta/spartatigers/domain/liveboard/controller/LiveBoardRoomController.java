787db9fc7c77607267a6cd4a412a19459ad8760b
harinbeee <luv_lyn@naver.com>
2025-06-11 16:52:01 +0900

    feat : 라이브보드 채팅방 생성, 삭제 api 구현 #74

--- src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardRoomController.java
+++ src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardRoomController.java
@@ -2,24 +2,44 @@ package com.sparta.spartatigers.domain.liveboard.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 import com.sparta.spartatigers.domain.liveboard.dto.response.LiveBoardRoomResponseDto;
-import com.sparta.spartatigers.domain.liveboard.service.LiveBoardService;
+import com.sparta.spartatigers.domain.liveboard.service.LiveBoardRoomService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/liveboard")
+@RequestMapping("/api/liveboard/room")
 public class LiveBoardRoomController {
 
-    private final LiveBoardService liveBoardService;
+    private final LiveBoardRoomService liveBoardRoomService;
 
-    @GetMapping("/rooms")
-    public List<LiveBoardRoomResponseDto> getRooms() {
-        return liveBoardService.findAllRoom();
+	@PostMapping
+	public List<LiveBoardRoomResponseDto> createTodayRoom() {
+		return liveBoardRoomService.createTodayRoom();
+	}
+
+    @GetMapping("/all")
+    public List<LiveBoardRoomResponseDto> getAllRooms() {
+        return liveBoardRoomService.findAllRooms();
     }
+
+	@GetMapping("/today")
+	public List<LiveBoardRoomResponseDto> getTodayRooms() {
+		return liveBoardRoomService.findTodayRooms();
+	}
+
+	@DeleteMapping("/{roomId}")
+	public void deleteRoom(
+		@PathVariable String roomId
+	) {
+		liveBoardRoomService.deleteRoom(roomId);
+	}
 }
