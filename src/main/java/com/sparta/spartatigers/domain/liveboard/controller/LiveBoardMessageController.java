f0225bf96195b1d27f035af7eda0031ce218d3e8
hyun2y00 <pottq577@naver.com>
2025-06-12 15:20:49 +0900

    feat: 로그 추가

--- src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardMessageController.java
+++ src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardMessageController.java
@@ -2,15 +2,20 @@ package com.sparta.spartatigers.domain.liveboard.controller;
 
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.sparta.spartatigers.domain.liveboard.model.LiveBoardMessage;
 import com.sparta.spartatigers.domain.liveboard.pubsub.RedisMessagePublisher;
 import com.sparta.spartatigers.domain.liveboard.service.LiveBoardService;
+import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class LiveBoardMessageController {
@@ -20,7 +25,10 @@ public class LiveBoardMessageController {
     private final SimpMessageSendingOperations messagingTemplate;
 
     @MessageMapping("/liveboard/message")
-    public void sendMessage(LiveBoardMessage message) {
+    public void sendMessage(
+            @Payload LiveBoardMessage message,
+            @AuthenticationPrincipal CustomUserPrincipal principal) {
+        log.info("sender id: {}", principal.getUsername());
         String roomId = message.getRoomId();
         ChannelTopic topic = liveBoardService.getTopic(roomId);
         if (topic == null) {
--- src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardRoomController.java
+++ src/main/java/com/sparta/spartatigers/domain/liveboard/controller/LiveBoardRoomController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.sparta.spartatigers.domain.liveboard.dto.response.LiveBoardRoomResponseDto;
 import com.sparta.spartatigers.domain.liveboard.service.LiveBoardService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/liveboard")
@@ -20,6 +22,10 @@ public class LiveBoardRoomController {
 
     @GetMapping("/rooms")
     public List<LiveBoardRoomResponseDto> getRooms() {
+        List<LiveBoardRoomResponseDto> allRooms = liveBoardService.findAllRoom();
+        for (LiveBoardRoomResponseDto allRoom : allRooms) {
+            log.info("rooms: {}", allRoom);
+        }
         return liveBoardService.findAllRoom();
     }
 }
