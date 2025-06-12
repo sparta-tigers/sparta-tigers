0aa97728146f27e99373a15bd2ed0411324ce012
anyubinn <anyubin0724@naver.com>
2025-06-12 15:45:07 +0900

    feat: SecurityConfig 수정 #75

--- src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.sparta.spartatigers.global.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.sparta.spartatigers.domain.user.service.CustomOAuth2UserDetailsService;
+import com.sparta.spartatigers.domain.user.service.CustomUserDetailsService;
+import com.sparta.spartatigers.global.filter.JwtFilter;
+import com.sparta.spartatigers.global.handler.OAuth2SuccessHandler;
+import com.sparta.spartatigers.global.util.JwtUtil;
 import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,76 +24,72 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lombok.RequiredArgsConstructor;
-
-import com.sparta.spartatigers.domain.user.service.CustomOAuth2UserDetailsService;
-import com.sparta.spartatigers.domain.user.service.CustomUserDetailsService;
-import com.sparta.spartatigers.global.filter.JwtFilter;
-import com.sparta.spartatigers.global.handler.OAuth2SuccessHandler;
-import com.sparta.spartatigers.global.util.JwtUtil;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true) // preAuthorize 설정시
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtUtil jwtUtil;
-    private final CustomOAuth2UserDetailsService oAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final CustomUserDetailsService userDetailsService;
+	private final JwtUtil jwtUtil;
+	private final CustomOAuth2UserDetailsService oAuth2UserService;
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
+	private final CustomUserDetailsService userDetailsService;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/*").permitAll().anyRequest().authenticated())
-                .oauth2Login(
-                        oauth ->
-                                oauth.userInfoEndpoint(
-                                                userInfo ->
-                                                        userInfo.userService(
-                                                                oAuth2UserService)) // #2
-                                        // .defaultSuccessUrl("/auth/login-success") // #3
-                                        .successHandler(oAuth2SuccessHandler)
-                                        .failureUrl("/fail"))
-                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors(withDefaults())
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(
+				sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+			.authorizeHttpRequests(
+				auth ->
+					auth.requestMatchers("/**")
+						.permitAll()
+						.anyRequest()
+						.authenticated())
+			.oauth2Login(
+				oauth ->
+					oauth.userInfoEndpoint(
+							userInfo ->
+								userInfo.userService(
+									oAuth2UserService)) // #2
+						// .defaultSuccessUrl("/auth/login-success") // #3
+						.successHandler(oAuth2SuccessHandler)
+						.failureUrl("/fail"))
+			.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        http.logout(
-                logout ->
-                        logout.logoutUrl("/api/users/logout") // 클라이언트가 이 경로로 POST하면 로그아웃 수행
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
-                                .permitAll());
+		http.logout(
+			logout ->
+				logout.logoutUrl("/api/users/logout") // 클라이언트가 이 경로로 POST하면 로그아웃 수행
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID")
+					.permitAll());
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
 
-        return source;
-    }
+		return source;
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-            throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+		throws Exception {
+		return configuration.getAuthenticationManager();
+	}
 
-    @Bean
-    public JwtFilter jwtFilter() {
-        return new JwtFilter(jwtUtil, userDetailsService);
-    }
+	@Bean
+	public JwtFilter jwtFilter() {
+		return new JwtFilter(jwtUtil, userDetailsService);
+	}
 }
