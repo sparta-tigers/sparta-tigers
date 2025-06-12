c5f3360c96eaf51a70fb24818b28daa274f4abbd
xaehub <novxae@gmail.com>
2025-06-12 14:32:29 +0900

    [feat/chatmessage] 채팅 메시지 기능 구현 #12 (#76)
    - refactor (1대1 채팅방): response DTO 이름 수정
    - feat (1대1 DM): 메시지 전송 기능 구현v1
    - feat (1대1 채팅): 1대1 채팅 기능 구현
    - 카카오 계정이 한개라 receiver를 가짜유저 1로 하드코딩해서 수신하는 것까지 테스트 완료
    - fix: git 최신화 websocket쪽 병합 충돌 해결v2
    - fix: RedisTemplate 빈 충돌 해결
    - CORS origin(http://localhost:63342) 허용 추가
    - feat(1대1 채팅): 채팅 메시지 기능 구현
    - userId: 2로 하드 코딩 됐던 거 제거
    - StompAuthInterceptor에서 JWT 인증 및 사용자 정보 추출
    - WebSocket 연결 시 userId와 sessionId를 UserSessionRegistry에 등록
    - 연결 해제(SessionDisconnectEvent) 시 세션 제거 처리
    - 사용자 연결 여부 확인을 위한 UserSessionRegistry 구현
    - RedisDirectMessageSubscriber에서 사용자 연결 여부 확인 후 메시지 전송
    - STOMP Principal 커스텀(StompPrincipal) 구현 및 세팅
    - WebSocket 연결 시 Authorization 헤더 출력(디버깅 용도)
    - RedisMessageListenerContainer에서 하드코딩됐던 부분 제거
    - fix: 최종 수정 후 린트 적용
    - feat (1대1 채팅): Redis 기반 분산 서버에서도 지원 가능한 UserSessionRegistry구현 및 Websocket 세션 관리 개선
    - 기존 UserSessionRegistry를 Redis를 활용한 RedisUserSessionRegistry로 대체
    - 사용자 ID와 세션 ID 매핑을 Redis Hash로 저장하여 다중 서버 환경에서 세션 공유 가능하게 개선
    - WebSocket 세션 연결 및 종료 이벤트 처리 로직 추가
    - 세션 등록/해제 시 Redis 데이터 자동 만료 기간 적용 TTL
    - 주석 살짝만 추가
    - docs (1대1 채팅): 간단한 코드 수정 및 주석 추가
    - fix (1대1 채팅): ObjectMapper 전역 빈 주입으로 생성 비용 최적화

---
+++ src/main/java/com/sparta/spartatigers/domain/chatroom/config/RedisDirectRoomMessageSubscriberConfig.java
@@ -0,0 +1,33 @@
+package com.sparta.spartatigers.domain.chatroom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.sparta.spartatigers.domain.chatroom.pubsub.RedisDirectMessageSubscriber;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RedisDirectRoomMessageSubscriberConfig {
+
+    private final RedisConnectionFactory connectionFactory;
+    private final RedisDirectMessageSubscriber subscriber;
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        // 모든 directroom:{id} 형식의 채널 구독
+        container.addMessageListener(subscriber, new PatternTopic("directroom:*"));
+        log.info("연결된 채팅방: directroom:*");
+
+        return container;
+    }
+}
