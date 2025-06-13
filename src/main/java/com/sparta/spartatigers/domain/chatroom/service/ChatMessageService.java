43c5a65bc00582711d0c5b6ab1b48d392eb6d468
xaehub <novxae@gmail.com>
2025-06-13 12:22:10 +0900

    feat (1대1 채팅): 특정 채팅방 메시지 조회 기능 구현 #13 (#80)
    - fix: JPQL order by절 제거
    - Pageable 사용해서 최신순으로 정렬

--- src/main/java/com/sparta/spartatigers/domain/chatroom/service/ChatMessageService.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.spartatigers.domain.chatroom.service;
-
-import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class ChatMessageService {}
