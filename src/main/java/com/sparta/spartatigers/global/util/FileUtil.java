fd4659bfa7abdbd3248d812b402fd4dd277c401e
aksirjwn <skks1000@naver.com>
2025-06-23 15:13:01 +0900

    feature : S3 유저 이미지 등록 및 삭제

---
+++ src/main/java/com/sparta/spartatigers/global/util/FileUtil.java
@@ -0,0 +1,56 @@
+package com.sparta.spartatigers.global.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
+import com.sparta.spartatigers.global.exception.ExceptionCode;
+import com.sparta.spartatigers.global.exception.ServerException;
+
+@Component
+@RequiredArgsConstructor
+public class FileUtil {
+    private final S3Properties s3Properties;
+
+    public String createFileName(String folderPath, String originalFileName) {
+        String uuidSubstring = UUID.randomUUID().toString().substring(0, 5);
+        String safeOriginalName = originalFileName.replaceAll("\\s+", "_");
+        return folderPath + uuidSubstring + "_" + safeOriginalName;
+    }
+
+    public String extractS3KeyFromUrl(String url) {
+        try {
+            URL s3Url = new URL(url);
+            String rawPath = s3Url.getPath().substring(1); // /user/xxx.png → user/xxx.png
+            return URLDecoder.decode(rawPath, StandardCharsets.UTF_8);
+        } catch (MalformedURLException e) {
+            throw new ServerException(ExceptionCode.FILE_DELETE_FAILED);
+        }
+    }
+
+    public void validateExtension(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null || !originalFilename.contains(".")) {
+            throw new ServerException(ExceptionCode.INVALID_FILE_FORMAT);
+        }
+
+        String ext =
+                originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
+        if (!s3Properties.getUpload().getAllowedExtensions().contains(ext)) {
+            throw new ServerException(ExceptionCode.INVALID_FILE_FORMAT);
+        }
+    }
+
+    public void validateFileSize(MultipartFile file) {
+        if (file.getSize() > s3Properties.getUpload().getMaxSize()) {
+            throw new ServerException(ExceptionCode.FILE_SIZE_EXCEEDED);
+        }
+    }
+}
