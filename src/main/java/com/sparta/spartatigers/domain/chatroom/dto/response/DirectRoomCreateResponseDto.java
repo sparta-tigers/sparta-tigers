464a91a547839262ff405304537477c7241ed579
xaehub <novxae@gmail.com>
2025-06-26 15:33:24 +0900

    refactor (1대1 채팅): 채팅방 생성 시 응답 데이터 수정

--- src/main/java/com/sparta/spartatigers/domain/chatroom/dto/response/DirectRoomCreateResponseDto.java
+++ src/main/java/com/sparta/spartatigers/domain/chatroom/dto/response/DirectRoomCreateResponseDto.java
@@ -1,36 +1,32 @@
 package com.sparta.spartatigers.domain.chatroom.dto.response;
 
+import com.sparta.spartatigers.domain.chatroom.model.entity.DirectRoom;
 import java.time.LocalDateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import com.sparta.spartatigers.domain.chatroom.model.entity.DirectRoom;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DirectRoomCreateResponseDto {
 
-    private Long directRoomId;
-    private Long exchangeRequestId;
-    private Long senderId;
-    private Long receiverId;
-    private String receiverNickname;
-    private boolean isCompleted;
-    private LocalDateTime completedAt;
-    private LocalDateTime createdAt;
+	private Long directRoomId;
+	private Long exchangeRequestId;
+	private Long senderId;
+	private Long receiverId;
+	private boolean isCompleted;
+	private LocalDateTime completedAt;
+	private LocalDateTime createdAt;
 
-    public static DirectRoomCreateResponseDto from(DirectRoom room) {
-        return new DirectRoomCreateResponseDto(
-                room.getId(),
-                room.getExchangeRequest().getId(),
-                room.getSender().getId(),
-                room.getReceiver().getId(),
-                room.getReceiver().getNickname(),
-                room.isCompleted(),
-                room.getCompletedAt(),
-                room.getCreatedAt());
-    }
+	public static DirectRoomCreateResponseDto from(DirectRoom room) {
+		return new DirectRoomCreateResponseDto(
+			room.getId(),
+			room.getExchangeRequest().getId(),
+			room.getSender().getId(),
+			room.getReceiver().getId(),
+			room.isCompleted(),
+			room.getCompletedAt(),
+			room.getCreatedAt());
+	}
 }
