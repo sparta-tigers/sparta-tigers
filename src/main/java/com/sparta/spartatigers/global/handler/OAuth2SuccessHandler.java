c70aaa4e62ca81945b84c248d696a78ec0dad7f0
jungmini <jungmini0601@gmail.com>
2025-06-30 07:16:55 +0900

    refactor: prod 환경으로 Redirect URL 변경

--- src/main/java/com/sparta/spartatigers/global/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/sparta/spartatigers/global/handler/OAuth2SuccessHandler.java
@@ -2,52 +2,51 @@ package com.sparta.spartatigers.global.handler;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
 import com.sparta.spartatigers.domain.user.model.entity.User;
 import com.sparta.spartatigers.domain.user.repository.UserRepository;
 import com.sparta.spartatigers.global.util.JwtUtil;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 @Component
 @Log4j2
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
-    private final JwtUtil jwtUtil;
-    private final UserRepository userRepository;
+	private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
 
-    @Override
-    public void onAuthenticationSuccess(
-            HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-            throws IOException, ServletException {
-        CustomUserPrincipal customUser = (CustomUserPrincipal) authentication.getPrincipal();
+	@Override
+	public void onAuthenticationSuccess(
+		HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+		throws IOException, ServletException {
+		CustomUserPrincipal customUser = (CustomUserPrincipal)authentication.getPrincipal();
 
-        String provider = customUser.getProvider();
-        String providerId = customUser.getUser().getProviderId();
-        String email = customUser.getUser().getEmail();
-        String nickname = customUser.getUser().getNickname();
-        String path = customUser.getUser().getPath();
+		String provider = customUser.getProvider();
+		String providerId = customUser.getUser().getProviderId();
+		String email = customUser.getUser().getEmail();
+		String nickname = customUser.getUser().getNickname();
+		String path = customUser.getUser().getPath();
 
-        log.info("email: {}", email);
-        log.info("nickname: {}", nickname);
-        log.info("path: {}", path);
+		log.info("email: {}", email);
+		log.info("nickname: {}", nickname);
+		log.info("path: {}", path);
 
-        String token = jwtUtil.generateToken(email, "USER");
+		String token = jwtUtil.generateToken(email, "USER");
 
-        if (!userRepository.existsByProviderId(providerId)) {
-            User newUser = User.from(provider, providerId, email, nickname, path);
-            userRepository.save(newUser);
-        }
+		if (!userRepository.existsByProviderId(providerId)) {
+			User newUser = User.from(provider, providerId, email, nickname, path);
+			userRepository.save(newUser);
+		}
 
-        response.sendRedirect("http://localhost:5173/oauth2/redirect?token=" + token);
-    }
+		response.sendRedirect("https://fe.yaguniv.site/redirect?token=" + token);
+	}
 }
