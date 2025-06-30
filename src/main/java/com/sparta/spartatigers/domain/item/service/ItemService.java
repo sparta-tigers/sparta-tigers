54a87753bc8ae4f5b0d224e2777c44740aee5681
anyubinn <anyubin0724@naver.com>
2025-06-30 09:26:06 +0900

    fix: 린트 적용 #119

--- src/main/java/com/sparta/spartatigers/domain/item/service/ItemService.java
+++ src/main/java/com/sparta/spartatigers/domain/item/service/ItemService.java
@@ -1,5 +1,15 @@
 package com.sparta.spartatigers.domain.item.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
 import com.sparta.spartatigers.domain.item.dto.request.CreateItemWithLocationRequestDto;
 import com.sparta.spartatigers.domain.item.dto.response.CreateItemResponseDto;
 import com.sparta.spartatigers.domain.item.dto.response.ReadItemDetailResponseDto;
@@ -12,71 +22,64 @@ import com.sparta.spartatigers.domain.user.model.CustomUserPrincipal;
 import com.sparta.spartatigers.domain.user.model.entity.User;
 import com.sparta.spartatigers.global.service.S3Service;
 import com.sparta.spartatigers.global.util.S3FolderType;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
-	private static final double SEARCH_RADIUS_KM = 0.05;
-	private final ItemRepository itemRepository;
-	private final LocationService locationService;
-	private final S3Service s3Service;
+    private static final double SEARCH_RADIUS_KM = 0.05;
+    private final ItemRepository itemRepository;
+    private final LocationService locationService;
+    private final S3Service s3Service;
 
-	@Transactional
-	public CreateItemResponseDto createItem(
-		CreateItemWithLocationRequestDto request,
-		MultipartFile file,
-		CustomUserPrincipal principal) {
+    @Transactional
+    public CreateItemResponseDto createItem(
+            CreateItemWithLocationRequestDto request,
+            MultipartFile file,
+            CustomUserPrincipal principal) {
 
-//        LocationRequestDto locationDto = request.getLocationDto();
-//        boolean isNear =
-//                locationService.isNearStadium(
-//                        locationDto.getLongitude(), locationDto.getLatitude());
-//
-//        if (!isNear) {
-//            throw new ServerException(ExceptionCode.LOCATION_NOT_VALID);
-//        }
+        //        LocationRequestDto locationDto = request.getLocationDto();
+        //        boolean isNear =
+        //                locationService.isNearStadium(
+        //                        locationDto.getLongitude(), locationDto.getLatitude());
+        //
+        //        if (!isNear) {
+        //            throw new ServerException(ExceptionCode.LOCATION_NOT_VALID);
+        //        }
 
-		String image = s3Service.uploadFile(file, S3FolderType.ITEM, principal.getUser().getId());
+        String image = s3Service.uploadFile(file, S3FolderType.ITEM, principal.getUser().getId());
 
-		User user = principal.getUser();
+        User user = principal.getUser();
 
-		Item item = Item.of(request.getItemDto(), user, image);
-		itemRepository.save(item);
+        Item item = Item.of(request.getItemDto(), user, image);
+        itemRepository.save(item);
 
-		ReadItemResponseDto newItemDto = ReadItemResponseDto.from(item);
-		locationService.notifyUsersNearBy(user.getId(), "ADD_ITEM", newItemDto);
+        ReadItemResponseDto newItemDto = ReadItemResponseDto.from(item);
+        locationService.notifyUsersNearBy(user.getId(), "ADD_ITEM", newItemDto);
 
-		return CreateItemResponseDto.from(item);
-	}
+        return CreateItemResponseDto.from(item);
+    }
 
-	@Transactional(readOnly = true)
-	public Page<ReadItemResponseDto> findAllItems(
-		CustomUserPrincipal principal, Pageable pageable) {
+    @Transactional(readOnly = true)
+    public Page<ReadItemResponseDto> findAllItems(
+            CustomUserPrincipal principal, Pageable pageable) {
 
-		Long userId = principal.getUser().getId();
+        Long userId = principal.getUser().getId();
 
-		List<Long> nearByUserIds = locationService.findUsersNearBy(userId, SEARCH_RADIUS_KM);
-		nearByUserIds.add(userId);
+        List<Long> nearByUserIds = locationService.findUsersNearBy(userId, SEARCH_RADIUS_KM);
+        nearByUserIds.add(userId);
 
-		Page<ReadItemFlatResponseDto> itemList =
-			itemRepository.findAllByStatus(Status.REGISTERED, nearByUserIds, pageable);
+        Page<ReadItemFlatResponseDto> itemList =
+                itemRepository.findAllByStatus(Status.REGISTERED, nearByUserIds, pageable);
 
-		return itemList.map(ReadItemResponseDto::from);
-	}
+        return itemList.map(ReadItemResponseDto::from);
+    }
 
-	@Transactional(readOnly = true)
-	public ReadItemDetailResponseDto findItemById(Long itemId) {
+    @Transactional(readOnly = true)
+    public ReadItemDetailResponseDto findItemById(Long itemId) {
 
-		Item item = itemRepository.findItemByIdOrElseThrow(itemId);
+        Item item = itemRepository.findItemByIdOrElseThrow(itemId);
 
-		return ReadItemDetailResponseDto.from(item);
-	}
+        return ReadItemDetailResponseDto.from(item);
+    }
 }
