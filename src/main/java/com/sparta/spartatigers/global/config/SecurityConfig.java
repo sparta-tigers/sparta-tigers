934f8b63e605a0f02f5f83e6fd7315c3d732acd8
jungmini <jungmini0601@gmail.com>
2025-07-01 14:01:20 +0900

    hotfix: AlarmController 및 SecurityConfig Logout 관련 설정

--- src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/spartatigers/global/config/SecurityConfig.java
@@ -21,13 +21,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lombok.RequiredArgsConstructor;
-
 import com.sparta.spartatigers.domain.user.service.CustomOAuth2UserDetailsService;
 import com.sparta.spartatigers.domain.user.service.CustomUserDetailsService;
 import com.sparta.spartatigers.global.filter.JwtFilter;
 import com.sparta.spartatigers.global.handler.OAuth2SuccessHandler;
 import com.sparta.spartatigers.global.util.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -35,71 +35,64 @@ import com.sparta.spartatigers.global.util.JwtUtil;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtUtil jwtUtil;
-    private final CustomOAuth2UserDetailsService oAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final CustomUserDetailsService userDetailsService;
+	private final JwtUtil jwtUtil;
+	private final CustomOAuth2UserDetailsService oAuth2UserService;
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
+	private final CustomUserDetailsService userDetailsService;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .oauth2Login(
-                        oauth ->
-                                oauth.userInfoEndpoint(
-                                                userInfo ->
-                                                        userInfo.userService(
-                                                                oAuth2UserService)) // #2
-                                        // .defaultSuccessUrl("/auth/login-success") // #3
-                                        .successHandler(oAuth2SuccessHandler)
-                                        .failureUrl("/fail"))
-                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors(withDefaults())
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(
+				sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.oauth2Login(
+				oauth ->
+					oauth.userInfoEndpoint(
+							userInfo ->
+								userInfo.userService(
+									oAuth2UserService)) // #2
+						// .defaultSuccessUrl("/auth/login-success") // #3
+						.successHandler(oAuth2SuccessHandler)
+						.failureUrl("/fail"))
+			.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
 
-        http.logout(
-                logout ->
-                        logout.logoutUrl("/api/users/logout")
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
-                                .permitAll());
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(
+			List.of(
+				"http://localhost:5173",
+				"http://localhost:63342",
+				"http://sparta-tigers-fe.s3-website.ap-northeast-2.amazonaws.com",
+				"https://fe.yaguniv.site"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
 
-        return http.build();
-    }
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(
-                List.of(
-                        "http://localhost:5173",
-                        "http://localhost:63342",
-                        "http://sparta-tigers-fe.s3-website.ap-northeast-2.amazonaws.com",
-                        "https://fe.yaguniv.site"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+		return source;
+	}
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+		throws Exception {
+		return configuration.getAuthenticationManager();
+	}
 
-        return source;
-    }
+	@Bean
+	public JwtFilter jwtFilter() {
+		return new JwtFilter(jwtUtil, userDetailsService);
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-            throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public JwtFilter jwtFilter() {
-        return new JwtFilter(jwtUtil, userDetailsService);
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
