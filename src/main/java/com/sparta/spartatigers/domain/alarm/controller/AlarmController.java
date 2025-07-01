be1f7aa60f124073df0b4f46cd56c38921eb632f
jungmini <jungmini0601@gmail.com>
2025-07-01 14:04:58 +0900

    style: 린트 수정

--- src/main/java/com/sparta/spartatigers/domain/alarm/controller/AlarmController.java
+++ src/main/java/com/sparta/spartatigers/domain/alarm/controller/AlarmController.java
@@ -2,6 +2,10 @@ package com.sparta.spartatigers.domain.alarm.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import com.sparta.spartatigers.domain.alarm.dto.request.AlarmRegisterDto;
 import com.sparta.spartatigers.domain.alarm.dto.request.AlarmUpdateDto;
@@ -30,103 +37,98 @@ import com.sparta.spartatigers.global.response.MessageCode;
 import com.sparta.spartatigers.global.util.JwtUtil;
 
 import io.jsonwebtoken.Claims;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/alarms")
 public class AlarmController {
-	private final AlarmService alarmService;
-	private final JwtUtil jwtUtil;
-	private final UserService userService;
+    private final AlarmService alarmService;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
 
-	// 알람 조회 (O)
-	@GetMapping
-	public ApiResponse<List<AlarmResponseDto>> getAllAlarms(
-		@AuthenticationPrincipal CustomUserPrincipal userPrincipal) {
-		List<AlarmResponseDto> response =
-			alarmService.findMyAlarms(userPrincipal.getUser().getId());
-		return ApiResponse.ok(response);
-	}
+    // 알람 조회 (O)
+    @GetMapping
+    public ApiResponse<List<AlarmResponseDto>> getAllAlarms(
+            @AuthenticationPrincipal CustomUserPrincipal userPrincipal) {
+        List<AlarmResponseDto> response =
+                alarmService.findMyAlarms(userPrincipal.getUser().getId());
+        return ApiResponse.ok(response);
+    }
 
-	// 알람 생성 (O)
-	@PostMapping
-	public ApiResponse<String> createAlarm(
-		@AuthenticationPrincipal CustomUserPrincipal userPrincipal,
-		@Valid @RequestBody AlarmRegisterDto alarmRegisterDto) {
+    // 알람 생성 (O)
+    @PostMapping
+    public ApiResponse<String> createAlarm(
+            @AuthenticationPrincipal CustomUserPrincipal userPrincipal,
+            @Valid @RequestBody AlarmRegisterDto alarmRegisterDto) {
 
-		alarmService.createAlarm(userPrincipal.getUser().getId(), alarmRegisterDto);
+        alarmService.createAlarm(userPrincipal.getUser().getId(), alarmRegisterDto);
 
-		return ApiResponse.created(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
-	}
+        return ApiResponse.created(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
+    }
 
-	// 알람 수정 (O)
-	@PutMapping
-	public ApiResponse<String> updateAlarm(
-		@AuthenticationPrincipal CustomUserPrincipal userPrincipal,
-		@Valid @RequestBody AlarmUpdateDto alarmUpdateDto) {
-		alarmService.updateAlarm(userPrincipal.getUser().getId(), alarmUpdateDto);
-		return ApiResponse.ok(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
-	}
+    // 알람 수정 (O)
+    @PutMapping
+    public ApiResponse<String> updateAlarm(
+            @AuthenticationPrincipal CustomUserPrincipal userPrincipal,
+            @Valid @RequestBody AlarmUpdateDto alarmUpdateDto) {
+        alarmService.updateAlarm(userPrincipal.getUser().getId(), alarmUpdateDto);
+        return ApiResponse.ok(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
+    }
 
-	// 알람 삭제 (O)
-	@DeleteMapping("/{alarmId}")
-	public ApiResponse<String> deleteAlarm(
-		@AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
-		@PathVariable Long alarmId) {
-		alarmService.deleteAlarm(customUserPrincipal.getUser().getId(), alarmId);
-		return ApiResponse.ok(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
-	}
+    // 알람 삭제 (O)
+    @DeleteMapping("/{alarmId}")
+    public ApiResponse<String> deleteAlarm(
+            @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+            @PathVariable Long alarmId) {
+        alarmService.deleteAlarm(customUserPrincipal.getUser().getId(), alarmId);
+        return ApiResponse.ok(MessageCode.ALARM_REQUEST_SUCCESS.getMessage());
+    }
 
-	// 팀 이름 조회 (O)
-	@GetMapping("/teams")
-	public ApiResponse<List<TeamNameResponseDto>> getAllTeamNames() {
-		return ApiResponse.ok(alarmService.findTeamNames());
-	}
+    // 팀 이름 조회 (O)
+    @GetMapping("/teams")
+    public ApiResponse<List<TeamNameResponseDto>> getAllTeamNames() {
+        return ApiResponse.ok(alarmService.findTeamNames());
+    }
 
-	// 월별 팀 일정 조회 (O)
-	@GetMapping("/teams/{teamId}/schedules")
-	public ApiResponse<List<MatchScheduleResponseDto>> getMatchSchedule(
-		@PathVariable Long teamId,
-		@RequestParam @Min(2020) @Max(2100) int year,
-		@RequestParam @Min(1) @Max(12) int month) {
-		List<MatchScheduleResponseDto> responseDtos =
-			alarmService.getMatchScheduleByTeamId(teamId, year, month);
-		return ApiResponse.ok(responseDtos);
-	}
+    // 월별 팀 일정 조회 (O)
+    @GetMapping("/teams/{teamId}/schedules")
+    public ApiResponse<List<MatchScheduleResponseDto>> getMatchSchedule(
+            @PathVariable Long teamId,
+            @RequestParam @Min(2020) @Max(2100) int year,
+            @RequestParam @Min(1) @Max(12) int month) {
+        List<MatchScheduleResponseDto> responseDtos =
+                alarmService.getMatchScheduleByTeamId(teamId, year, month);
+        return ApiResponse.ok(responseDtos);
+    }
 
-	// 팀 일정 세부 조회 (O)
-	@GetMapping("/matches/{matchId}")
-	public ApiResponse<MatchDetailResponseDto> getMatchScheduleDetails(@PathVariable Long matchId) {
-		MatchDetailResponseDto responseDtos = alarmService.getMatchByMatchId(matchId);
-		return ApiResponse.ok(responseDtos);
-	}
+    // 팀 일정 세부 조회 (O)
+    @GetMapping("/matches/{matchId}")
+    public ApiResponse<MatchDetailResponseDto> getMatchScheduleDetails(@PathVariable Long matchId) {
+        MatchDetailResponseDto responseDtos = alarmService.getMatchByMatchId(matchId);
+        return ApiResponse.ok(responseDtos);
+    }
 
-	@GetMapping("/sse/subscribe")
-	public SseEmitter subscribe(@RequestParam String token) {
+    @GetMapping("/sse/subscribe")
+    public SseEmitter subscribe(@RequestParam String token) {
 
-		Claims claims = jwtUtil.validateToken(token);
-		System.out.println(claims);
-		if (claims == null) {
-			throw new ServerException(ExceptionCode.NOT_FOUND_JWT);
-		}
+        Claims claims = jwtUtil.validateToken(token);
+        System.out.println(claims);
+        if (claims == null) {
+            throw new ServerException(ExceptionCode.NOT_FOUND_JWT);
+        }
 
-		String email = claims.getSubject();
+        String email = claims.getSubject();
 
-		Long userId = userService.findUserIdByEmail(email);
+        Long userId = userService.findUserIdByEmail(email);
 
-		return alarmService.subscribe(userId);
-	}
+        return alarmService.subscribe(userId);
+    }
 
-	// @GetMapping("/sse/subscribe")
-	// public SseEmitter subscribe(Authentication authentication) {
-	// 	CustomUserPrincipal principal = (CustomUserPrincipal)authentication.getPrincipal();
-	// 	Long userId = principal.getUser().getId();
-	// 	return alarmService.subscribe(userId);
-	// }
+    // @GetMapping("/sse/subscribe")
+    // public SseEmitter subscribe(Authentication authentication) {
+    // 	CustomUserPrincipal principal = (CustomUserPrincipal)authentication.getPrincipal();
+    // 	Long userId = principal.getUser().getId();
+    // 	return alarmService.subscribe(userId);
+    // }
 }
--- src/main/java/com/sparta/spartatigers/domain/user/service/UserService.java
+++ src/main/java/com/sparta/spartatigers/domain/user/service/UserService.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
+
 import com.sparta.spartatigers.domain.user.dto.request.LoginRequestDto;
 import com.sparta.spartatigers.domain.user.dto.request.SignUpRequestDto;
 import com.sparta.spartatigers.domain.user.dto.request.UpdateNicknameRequestDto;
@@ -24,158 +25,158 @@ import com.sparta.spartatigers.global.util.JwtUtil;
 import com.sparta.spartatigers.global.util.S3FolderType;
 import com.sparta.spartatigers.global.util.S3Properties;
 
-import lombok.RequiredArgsConstructor;
+import com.amazonaws.services.s3.AmazonS3;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-	private final PasswordEncoder passwordEncoder;
-	private final UserRepository userRepository;
-	private final JwtUtil jwtUtil;
-	private final AmazonS3 amazonS3;
-	private final S3Properties s3Properties;
-	private final FileUtil fileUtil;
-	private final S3Service s3Service;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final AmazonS3 amazonS3;
+    private final S3Properties s3Properties;
+    private final FileUtil fileUtil;
+    private final S3Service s3Service;
 
-	public UserInfoResponseDto getUserInfo(Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
-		return UserInfoResponseDto.from(user);
-	}
+    public UserInfoResponseDto getUserInfo(Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+        return UserInfoResponseDto.from(user);
+    }
 
-	@Transactional
-	public void createUser(SignUpRequestDto signUpRequestDto) {
-		String encodedPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
+    @Transactional
+    public void createUser(SignUpRequestDto signUpRequestDto) {
+        String encodedPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
 
-		if (userRepository.existsByEmail(signUpRequestDto.getEmail())) {
-			throw new ServerException(ExceptionCode.EMAIL_ALREADY_USED);
-		}
+        if (userRepository.existsByEmail(signUpRequestDto.getEmail())) {
+            throw new ServerException(ExceptionCode.EMAIL_ALREADY_USED);
+        }
 
-		if (userRepository.existsByNickname(signUpRequestDto.getNickname())) {
-			throw new ServerException(ExceptionCode.NICKNAME_ALREADY_USED);
-		}
+        if (userRepository.existsByNickname(signUpRequestDto.getNickname())) {
+            throw new ServerException(ExceptionCode.NICKNAME_ALREADY_USED);
+        }
 
-		User user = User.from(signUpRequestDto, encodedPassword);
-		userRepository.save(user);
-	}
+        User user = User.from(signUpRequestDto, encodedPassword);
+        userRepository.save(user);
+    }
 
-	@Transactional
-	public AuthResponseDto login(LoginRequestDto loginRequestDto) {
-		User user =
-			userRepository
-				.findByEmail(loginRequestDto.getEmail())
-				.orElseThrow(
-					() -> new InvalidRequestException(ExceptionCode.USER_NOT_FOUND));
-		if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
-			throw new InvalidRequestException(ExceptionCode.PASSWORD_NOT_MATCH);
-		}
-		String token = jwtUtil.generateToken(loginRequestDto.getEmail(), "ROLE_USER");
-		return AuthResponseDto.from(token);
-	}
+    @Transactional
+    public AuthResponseDto login(LoginRequestDto loginRequestDto) {
+        User user =
+                userRepository
+                        .findByEmail(loginRequestDto.getEmail())
+                        .orElseThrow(
+                                () -> new InvalidRequestException(ExceptionCode.USER_NOT_FOUND));
+        if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
+            throw new InvalidRequestException(ExceptionCode.PASSWORD_NOT_MATCH);
+        }
+        String token = jwtUtil.generateToken(loginRequestDto.getEmail(), "ROLE_USER");
+        return AuthResponseDto.from(token);
+    }
 
-	@Transactional
-	public ProfileResponseDto updateProfile(MultipartFile file, Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+    @Transactional
+    public ProfileResponseDto updateProfile(MultipartFile file, Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
 
-		String currentImagePath = user.getPath();
-		if (currentImagePath != null
-			&& !currentImagePath.endsWith(s3Properties.getDefaultImagePath())) {
-			try {
-				String key = fileUtil.extractS3KeyFromUrl(currentImagePath);
-				amazonS3.deleteObject(s3Properties.getBucket(), key);
-			} catch (Exception e) {
-				throw new ServerException(ExceptionCode.FILE_DELETE_FAILED);
-			}
-		}
+        String currentImagePath = user.getPath();
+        if (currentImagePath != null
+                && !currentImagePath.endsWith(s3Properties.getDefaultImagePath())) {
+            try {
+                String key = fileUtil.extractS3KeyFromUrl(currentImagePath);
+                amazonS3.deleteObject(s3Properties.getBucket(), key);
+            } catch (Exception e) {
+                throw new ServerException(ExceptionCode.FILE_DELETE_FAILED);
+            }
+        }
 
-		String folderPath = s3Properties.getFolderPath(S3FolderType.USER);
-		String originalFileName = file.getOriginalFilename();
-		String fileName = fileUtil.createFileName(folderPath, originalFileName, user.getId());
-		String filePath = s3Service.uploadFile(file, S3FolderType.USER, user.getId());
+        String folderPath = s3Properties.getFolderPath(S3FolderType.USER);
+        String originalFileName = file.getOriginalFilename();
+        String fileName = fileUtil.createFileName(folderPath, originalFileName, user.getId());
+        String filePath = s3Service.uploadFile(file, S3FolderType.USER, user.getId());
 
-		user.updatePath(filePath);
-		userRepository.save(user);
+        user.updatePath(filePath);
+        userRepository.save(user);
 
-		return new ProfileResponseDto(fileName, filePath);
-	}
+        return new ProfileResponseDto(fileName, filePath);
+    }
 
-	@Transactional
-	public void deleteProfile(Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+    @Transactional
+    public void deleteProfile(Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
 
-		String currentImagePath = user.getPath();
+        String currentImagePath = user.getPath();
 
-		if (currentImagePath.endsWith(s3Properties.getDefaultImagePath())) {
-			throw new InvalidRequestException(ExceptionCode.DEFAULT_IMAGE_CANNOT_BE_DELETED);
-		}
-		try {
-			String key = fileUtil.extractS3KeyFromUrl(currentImagePath);
-			amazonS3.deleteObject(s3Properties.getBucket(), key);
-		} catch (Exception e) {
-			throw new ServerException(ExceptionCode.FILE_DELETE_FAILED);
-		}
-		String defaultImageUrl =
-			amazonS3.getUrl(s3Properties.getBucket(), s3Properties.getDefaultImagePath())
-				.toString();
-		user.updatePath(defaultImageUrl);
-		userRepository.save(user);
-	}
+        if (currentImagePath.endsWith(s3Properties.getDefaultImagePath())) {
+            throw new InvalidRequestException(ExceptionCode.DEFAULT_IMAGE_CANNOT_BE_DELETED);
+        }
+        try {
+            String key = fileUtil.extractS3KeyFromUrl(currentImagePath);
+            amazonS3.deleteObject(s3Properties.getBucket(), key);
+        } catch (Exception e) {
+            throw new ServerException(ExceptionCode.FILE_DELETE_FAILED);
+        }
+        String defaultImageUrl =
+                amazonS3.getUrl(s3Properties.getBucket(), s3Properties.getDefaultImagePath())
+                        .toString();
+        user.updatePath(defaultImageUrl);
+        userRepository.save(user);
+    }
 
-	@Transactional
-	public void updateNickname(UpdateNicknameRequestDto updateNicknameRequestDto, Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+    @Transactional
+    public void updateNickname(UpdateNicknameRequestDto updateNicknameRequestDto, Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
 
-		user.updateNickname(updateNicknameRequestDto.getNickname());
-	}
+        user.updateNickname(updateNicknameRequestDto.getNickname());
+    }
 
-	@Transactional
-	public void updatePassword(UpdatePasswordRequestDto updatePasswordRequestDto, Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+    @Transactional
+    public void updatePassword(UpdatePasswordRequestDto updatePasswordRequestDto, Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
 
-		if (!passwordEncoder.matches(
-			updatePasswordRequestDto.getOldPassword(), user.getPassword())) {
-			throw new InvalidRequestException(ExceptionCode.INVALID_PASSWORD);
-		}
+        if (!passwordEncoder.matches(
+                updatePasswordRequestDto.getOldPassword(), user.getPassword())) {
+            throw new InvalidRequestException(ExceptionCode.INVALID_PASSWORD);
+        }
 
-		if (passwordEncoder.matches(
-			updatePasswordRequestDto.getNewPassword(), user.getPassword())) {
-			throw new InvalidRequestException(ExceptionCode.SAME_AS_OLD_PASSWORD);
-		}
+        if (passwordEncoder.matches(
+                updatePasswordRequestDto.getNewPassword(), user.getPassword())) {
+            throw new InvalidRequestException(ExceptionCode.SAME_AS_OLD_PASSWORD);
+        }
 
-		String encodedNewPassword =
-			passwordEncoder.encode(updatePasswordRequestDto.getNewPassword());
-		user.updatePassword(encodedNewPassword);
-	}
+        String encodedNewPassword =
+                passwordEncoder.encode(updatePasswordRequestDto.getNewPassword());
+        user.updatePassword(encodedNewPassword);
+    }
 
-	@Transactional
-	public void deleteUser(Long userId) {
-		User user =
-			userRepository
-				.findById(userId)
-				.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
+    @Transactional
+    public void deleteUser(Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND));
 
-		user.deleted();
-		userRepository.save(user);
-	}
+        user.deleted();
+        userRepository.save(user);
+    }
 
-	public Long findUserIdByEmail(String email) {
-		return userRepository
-			.findByEmail(email)
-			.orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND))
-			.getId();
-	}
+    public Long findUserIdByEmail(String email) {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new ServerException(ExceptionCode.USER_NOT_FOUND))
+                .getId();
+    }
 }
